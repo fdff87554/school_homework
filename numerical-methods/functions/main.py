238dca51d99020489d923e7506a43d31157f1c19
CrazyFire <fdff87554@gmail.com>
2021-06-09 12:31:52 +0800

    add cubic spline function

--- numerical-methods/functions/main.py
+++ numerical-methods/functions/main.py
@@ -1,19 +1,39 @@
+import matplotlib.pyplot as plt
 import numpy as np
+from numpy import exp, linspace, polyfit, poly1d
+from scipy.interpolate import CubicSpline
 
-from utils.interpolation import create_table, newton_forward, newton_backward, gauss_forward, gauss_backward
+from utils.interpolation import create_table, newton_forward, newton_backward, gauss_forward, gauss_backward, \
+    cubic_spline
 from utils.integration import trapezoid_rule, simpson_13_rule, simpson_38_rule
+# from utils.polynomial import polyfit
+
+
+def cubic_spline_test():
+    x = [0.0, 0.2, 0.4, 0.6, 0.8, 1.0]
+    y = [2.0, 2.008, 2.064, 2.216, 2.512, 3.0]
+
+    # print(cubic_spline(x, y))
+    f = CubicSpline(x, y, bc_type='natural')
+    xp = linspace(min(x) - 0.1, max(x) + 0.1, 100)
+    plt.plot(x, y, '.', xp, f(xp), '-')
+    plt.show()
 
 
 def polynomial_test():
     x = [0.0, 0.5, 1.0, 1.5, 2.0, 2.5]
     y = [0.0, 0.20, 0.27, 0.30, 0.32, 0.33]
 
-    print(np.polyfit(x, y, 3))
+    # print(polyfit(x, y, 3))
+    xp = linspace(min(x) - 0.1, max(x) + 0.1, 100)
+    f = poly1d(polyfit(x, y, 3))
+    plt.plot(x, y, '.', xp, f(xp), '-')
+    plt.show()
 
 
 def func(x):
 
-    return (x ** 2) * np.exp(x)
+    return (x ** 2) * exp(x)
 
 
 def integration_test():
@@ -36,6 +56,7 @@ def interpolation_test():
 
 
 if __name__ == '__main__':
-    interpolation_test()
-    integration_test()
-    polynomial_test()
+    # interpolation_test()
+    # integration_test()
+    # polynomial_test()
+    cubic_spline_test()
