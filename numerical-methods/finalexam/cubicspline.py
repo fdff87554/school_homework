8d1e61626a7a4f3d8b4a69c1441482d1c6b0ef7f
CrazyFire <fdff87554@gmail.com>
2021-06-17 15:26:31 +0800

    fix finalexam code

--- numerical-methods/finalexam/cubicspline.py
+++ numerical-methods/finalexam/cubicspline.py
@@ -42,15 +42,48 @@ def cubic_spline(x, y):
         table[i][i - 1] = h[i - 1]
         table[i][i] = 2 * (h[i - 1] + h[i])
         table[i][i + 1] = h[i]
-    print(table)
 
     c = round(linalg.inv(table).dot(v.reshape(len(x), 1)), 3).flatten()
     b = round([(1 / h[i]) * (a[i + 1] - a[i]) - (h[i] / 3) * (c[i + 1] + 2 * c[i]) for i in range(len(x) - 1)], 3)
     d = round([(c[i + 1] - c[i]) / (3 * h[i]) for i in range(len(x) - 1)], 3)
 
-    print(a[:len(b)], b, c[:len(b)], d)
+    return a[:len(b)], b, c[:len(b)], d
+
+
+def cubic_spline_tables(x, y):
+
+    x, y = array(x), array(y)
+    a = y
+    v = zeros(x.shape)
+    h = round([x[i + 1] - x[i] for i in range(len(x) - 1)], 3)
+    w = round([(1 / h[i]) * (a[i + 1] - a[i]) for i in range(len(a) - 1)], 3)
+    for i in range(1, len(w)):
+        v[i] = round(3 * (w[i] - w[i - 1]), 10)
+    table = zeros((len(x), len(x)))
+    table[0][0], table[len(x) - 1][len(x) - 1] = 1, 1
+    for i in range(1, len(x) - 1):
+        table[i][i - 1] = h[i - 1]
+        table[i][i] = 2 * (h[i - 1] + h[i])
+        table[i][i + 1] = h[i]
+    print(table)
+    c = round(linalg.inv(table).dot(v.reshape(len(x), 1)), 3)
+    print(c)
+    print(v.reshape(c.shape))
 
 
 if __name__ == '__main__':
+    # # test x, y
+    # x = [0.0, 0.2, 0.4, 0.6, 0.8, 1.0]
+    # y = [2.0, 2.008, 2.064, 2.216, 2.512, 3.0]
+    # a, b, c, d = cubic_spline(x, y)
+    # # output S_3 coefficient
+    # print(a[2], b[2], c[2], d[2])
+    # cubic_spline_tables(x, y)
     x, y = open_file()
-    cubic_spline(x[:6], y[:6])
+
+    # question 1, s_8 coefficient
+    a, b, c, d = cubic_spline(x, y)
+    print(a[7], b[7], c[7], d[7])
+
+    # question 2, tables
+    cubic_spline_tables(x[:6], y[:6])
